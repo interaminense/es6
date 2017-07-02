a1173573a5662fd336bd0b5912eefd1958a2343d
Adriano Interaminense <adriano.interaminense@gmail.com>
2017-07-02 02:17:12 -0300

    message service

--- image-message.model.js
+++ image-message.model.js
@@ -1,3 +1,5 @@
+const Message = require('./message.model');
+
 class ImageMessage extends Message {
   constructor(text = '', created = Date.now(),
     url = '', thumbnail = '') {
@@ -7,12 +9,14 @@ class ImageMessage extends Message {
   }
 
   /**
-   * Method overriden
-   * @returns String
-   */
+  * Method overriden
+  * @returns String
+  */
   toString() {
     return `Photo${super.toString()} ` +
-           `- Url: ${this.url} ` +
-           `- Thumbnail: ${this.thumbnail}`;
+      `- Url: ${this.url} ` +
+      `- Thumbnail: ${this.thumbnail}`;
   }
 }
+
+module.exports = ImageMessage;
